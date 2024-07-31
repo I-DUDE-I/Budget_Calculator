b890eb681bbb08de869eb4b958be736b5b438818
I-DUDE-I <thejusmathew8888@gmail.com>
2024-07-31 11:28:28 +0530

    dfv

--- script.js
+++ script.js
@@ -54,25 +54,196 @@ function register() {
         alert("Fill all the empty fields")
     }
     else {
-        localStorage.setItem("username",document.getElementById("registerUsername").value)
-        localStorage.setItem("email",document.getElementById("registerEmail").value)
-        localStorage.setItem("password",document.getElementById("registerPassword").value)
-        alert("User registered successfully")
-        document.getElementById("registerUsername").value = ""
-        document.getElementById("registerEmail").value = ""
-        document.getElementById("registerPassword").value = ""
+        emailReg = false
+        j =1
+        for(let i in localStorage) {
+            if (j > localStorage.length) {
+                break
+            }
+            if(JSON.parse(localStorage.getItem(i)).EMAIL == document.getElementById("registerEmail").value){
+                emailReg = true
+            }
+            j += 1
+        }
+        if(emailReg) {
+            alert("Email already registered")
+            document.getElementById("registerEmail").value = ""
+        }
+        else if (document.getElementById("registerUsername").value in localStorage) {
+            alert("Username already taken")
+            document.getElementById("registerUsername").value = ""
+        }
+        else {
+            array = {
+                EMAIL: document.getElementById("registerEmail").value,
+                PASSWORD : document.getElementById("registerPassword").value,
+                EXPENSEamount: [],
+                EXPENSEtype:[],
+                EXPENSEtime: [],
+                EXPENSEbalance: [],
+                INCOMEamount: [],
+                INCOMEtype:[],
+                INCOMEbalance:[],
+                INCOMEtime:[],
+                BALANCE:0
+            }
+            localStorage.setItem(document.getElementById("registerUsername").value,JSON.stringify(array))
+            alert("User registered successfully")
+            document.getElementById("registerUsername").value = ""
+            document.getElementById("registerEmail").value = ""
+            document.getElementById("registerPassword").value = ""
+        }
     }
 }
 
+
 function login() {
+    user = ""
     if (document.getElementById("loginUsername").value == "" || document.getElementById("loginPassword").value == "") {
         alert("Fill all the empty fields")
     }
-    else if(document.getElementById("loginUsername").value != localStorage.getItem("username") || document.getElementById("loginPassword").value != localStorage.getItem("password")) {
-        alert("Invalid user credentials")
+    else if(document.getElementById("loginUsername").value in localStorage) {
+        if (JSON.parse(localStorage.getItem(document.getElementById("loginUsername").value)).PASSWORD == document.getElementById("loginPassword").value) {
+            window.location = "./dashboard/index.html" 
+            user = document.getElementById("loginUsername").value
+            localStorage.setItem(",.12@" ,user)
+        }
+        else {
+            alert("Incorrect Password")
+            document.getElementById("loginPassword").value = ""
+        }
     }
     else{
-        alert("success")
+        alert("Invalid username")
+        document.getElementById("loginUsername").value = ""
     }
-    window.location = "./dashboard/index.html"
+}
+function clearr() {
+    localStorage.clear()
+}
+
+function logout() {
+    localStorage.removeItem(",.12@")
+    window.location="../index.html"
+}
+
+function addIncome() {
+    if (document.getElementById("incomeAmount").value == "" || document.getElementById("incomeType").value == "" ) {
+        alert("Fill the empty fields")
+    }
+    else{
+        user = localStorage.getItem(",.12@")
+        array = JSON.parse(localStorage.getItem(user))
+        balance = Number(array.BALANCE)
+        balance += Number(document.getElementById("incomeAmount").value)
+        array.INCOMEamount.push(document.getElementById("incomeAmount").value)
+        array.INCOMEtype.push(document.getElementById("incomeType").value)
+        array.BALANCE = balance
+        totalBalance.innerHTML = `Rs ${balance}/-`
+        let now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let seconds = now.getSeconds();
+
+        let dateObj = new Date();
+
+        let month = String(dateObj.getMonth() + 1)
+            .padStart(2, '0');
+            
+        let day = String(dateObj.getDate())
+            .padStart(2, '0');
+
+        let year = dateObj.getFullYear();
+
+        let output = day + '/' + month + '/' + year + " " + hours + ":" + minutes + ":" + seconds
+
+        array.INCOMEtime.push(output)
+        array.INCOMEbalance.push(balance)
+
+        localStorage.setItem(user,JSON.stringify(array))
+
+        htmlContent = `
+            <tr>
+                <td>${document.getElementById("incomeType").value}</td>
+                <td>${document.getElementById("incomeAmount").value}</td>
+                <td>${balance}</td>
+                <td>${output}</td>
+            </tr>
+        `
+        tbodyIncome.innerHTML += htmlContent
+        document.getElementById("incomeAmount").value =""
+        document.getElementById("incomeType").value =""
+    }
+}
+
+function addExpense(params) {
+    if (document.getElementById("expenseAmount").value == "" || document.getElementById("expenseType").value == "" ) {
+        alert("Fill the empty fields")
+    }
+    else{
+        user = localStorage.getItem(",.12@")
+        array = JSON.parse(localStorage.getItem(user))
+        balance = Number(array.BALANCE)
+        balance -= Number(document.getElementById("expenseAmount").value)
+        array.EXPENSEamount.push(document.getElementById("expenseAmount").value)
+        array.EXPENSEtype.push(document.getElementById("expenseType").value)
+        array.BALANCE = balance
+        totalBalance.innerHTML = `Rs ${balance}/-`
+        expense = array.EXPENSEamount.reduce((a,b) => Number(a) + Number(b),0)
+        totalExpense.innerHTML = `Rs ${expense}/-`
+        let now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let seconds = now.getSeconds();
+
+        let dateObj = new Date();
+
+        let month = String(dateObj.getMonth() + 1)
+            .padStart(2, '0');
+            
+        let day = String(dateObj.getDate())
+            .padStart(2, '0');
+
+        let year = dateObj.getFullYear();
+
+        let output = day + '/' + month + '/' + year + " " + hours + ":" + minutes + ":" + seconds
+
+        array.EXPENSEtime.push(output)
+        array.EXPENSEbalance.push(balance)
+
+        localStorage.setItem(user,JSON.stringify(array))
+
+        htmlContent = `
+            <tr>
+                <td>${document.getElementById("expenseType").value}</td>
+                <td>${document.getElementById("expenseAmount").value}</td>
+                <td>${balance}</td>
+                <td>${output}</td>
+            </tr>
+        `
+        tbodyExpense.innerHTML += htmlContent
+        document.getElementById("expenseAmount").value =""
+        document.getElementById("expenseType").value =""
+    }
+}
+
+function clearData() {
+    user = localStorage.getItem(",.12@")
+    array = JSON.parse(localStorage.getItem(user))
+    array.EXPENSEamount = []
+    array.EXPENSEbalance = []
+    array.EXPENSEtype = []
+    array.EXPENSEtime = []
+    array.BALANCE = 0
+    array.INCOMEamount = []
+    array.INCOMEbalance = []
+    array.INCOMEtime = []
+    array.INCOMEtype = []
+    if (confirm("Are you sure you want to clear the entire data")) {
+        localStorage.setItem(user,JSON.stringify(array))
+        location.reload()
+    }
+    else{
+        console.log(1);
+    }
 }
